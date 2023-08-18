85d9dd0e49cfd7132d808b7d8521817c5f3ecea8
Alejandro de la Rosa Antonio <alejandroantonio9715@gmail.com>
2023-08-17 19:54:04 -0600

    ajustes al valor de las monedas

--- src/conversorMonedas/ConvertirMonedasAPesos.java
+++ src/conversorMonedas/ConvertirMonedasAPesos.java
@@ -4,32 +4,32 @@ import javax.swing.JOptionPane;
 
 public class ConvertirMonedasAPesos {
     public void ConvertirDolaresAPesos(double valor) {
-        double monedaDolar = valor * 3739.00;
+        double monedaDolar = valor * 17.08;
         monedaDolar = (double) Math.round(monedaDolar *100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaDolar+ " Pesos Colombianos");
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaDolar+ " Pesos Méxicanos");
     }
 
     public void ConvertirEurosAPesos(double valor) {
-        double monedaEuro = valor * 4050.48;
+        double monedaEuro = valor * 18.61;
         monedaEuro = (double) Math.round(monedaEuro *100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaEuro+ " Pesos Colombianos");
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaEuro+ " Pesos Méxicanos");
     }
 
     public void ConvertirLibrasAPesos(double valor) {
-        double monedaLibra = valor * 4890.52;
+        double monedaLibra = valor * 21.80;
         monedaLibra = (double) Math.round(monedaLibra *100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaLibra+ " Pesos Colombianos");
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaLibra+ " Pesos Méxicanos");
     }
 
     public void ConvertirYenAPesos(double valor) {
-        double monedaYen = valor * 29.68;
+        double monedaYen = valor * 0.12;
         monedaYen = (double) Math.round(monedaYen *100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaYen+ " Pesos Colombianos");
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaYen+ " Pesos Méxicanos");
     }
 
     public void ConvertirWonAPesos(double valor) {
-        double monedaWon = valor * 3.04;
+        double monedaWon = valor * 0.013;
         monedaWon = (double) Math.round(monedaWon *100d)/100;
-        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaWon+ " Pesos Colombianos");
+        JOptionPane.showMessageDialog(null, "Tienes $ " +monedaWon+ " Pesos Méxicanos");
     }
 }
